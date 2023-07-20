919524c4172966860497d456eb4de9989390a80a
Pratham131 <104488785+Pratham131@users.noreply.github.com>
2023-07-20 09:52:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 function highlight() {
     //Write your code here
-
+	let boldText = document.querySelectorAll("strong")
+	boldText.forEach((element) => {
+		element.style.color = "green"
+	})
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	let boldText = document.querySelectorAll("strong")
+	boldText.forEach((element) => {
+		element.style.color = "black"
+	})
     
 }
+
+let link = document.getElementByTagName("a")[0]
+link.addEventListner('onMouseOver', highlight)
+link.addEventListner('onMouseOut', return_normal)
